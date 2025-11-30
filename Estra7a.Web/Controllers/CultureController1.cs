2b538f62a3d33b202496fb63d38c59d27b8d37f3
Mohamed <141072642+mahamednasser@users.noreply.github.com>
2025-11-30 20:35:01 +0200

    Add files via upload

---
+++ Estra7a.Web/Controllers/CultureController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estra7a.Web.Controllers
+{
+    public class CultureController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SetLanguage( string culture , string returnUrl )
+        {
+            if ( !string.IsNullOrEmpty(culture) )
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName ,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture , culture)) , // Set both culture and UICulture
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) , IsEssential = true }
+                );
+            }
+
+            return LocalRedirect(returnUrl ?? "/"); // Fallback to homepage if returnUrl is null
+        }
+    }
+}
