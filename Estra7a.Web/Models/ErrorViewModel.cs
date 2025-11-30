c8d7e2840648c27e0e1ffa41b2c637ce5fb1a0f3
Mohamed <141072642+mahamednasser@users.noreply.github.com>
2025-11-30 20:35:16 +0200

    Delete Estra7a.Web/Models directory

--- Estra7a.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Estra7a.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
